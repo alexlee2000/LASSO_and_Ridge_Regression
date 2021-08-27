29455278894b8ea527583db32c2478732f856c0d
Alex Lee <43845085+alexlee2000@users.noreply.github.com>
2021-08-27 10:27:08 +1000

    Update Q2_a_code.py

--- Q2_a_code.py
+++ Q2_a_code.py
@@ -5,24 +5,13 @@ import numpy as np
 from sklearn import datasets,linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
-# load the hw1 dataset
-df=pd.read_csv('data.csv')
+# load the dataset 
+data = pd.read_csv("data.csv")
+X = data.iloc[:,:-1]
+Y = data.iloc[:,-1]
 
-# Compute the correlation matrix
-corr = df.corr()
-
-# Generate a mask for the upper triangle
-mask = np.zeros_like(corr, dtype=np.bool)
-mask[np.triu_indices_from(mask)] = True #True values in upper triangle
-
-# Set up the matplotlib figure
-f, ax = plt.subplots(figsize=(11, 9))
-
-# Generate a custom diverging colormap
-cmap = sns.diverging_palette(220, 10, as_cmap=True)
-
-# Draw the heatmap with the mask and correct aspect ratio
-sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
-            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+sns.pairplot(data.iloc[:,:-1])
+plt.savefig("figures/PairsPlot.png", dpi=400)
 
 plt.show()
+
